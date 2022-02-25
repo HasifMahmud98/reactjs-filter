cbcfb203ff4b7c0cd21776786cb81c5d6f50a62e
HasifMahmud98 <hasifmahmud98@gmail.com>
2022-02-25 16:42:20 +0600

    Filter button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from "react";
 import Movie from './Componets/Movie';
 import Filter from './Componets/Filter';
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
   }, []);
 
   const fetchPopular = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=426c4ee9c1c43f7b4343c8b5617f358f&language=en-US&page=1');
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=426c4ee9c1c43f7b4343c8b5617f358f&language=en-US&page=[1,2,5]');
     const movies = await data.json();
     console.log(movies);
     setPopular(movies.results);
@@ -24,11 +25,16 @@ function App() {
   return (
     <div className="App">
       <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre}/>
-      <div className="popular-movies">
-        {popular.map(movie => {
-          return <Movie key={movie.id} movie={movie}/>
-        })}
-      </div>
+      <motion.div
+        layout 
+        className="popular-movies"
+      >
+        <AnimatePresence>
+          {filtered.map(movie => {
+            return <Movie key={movie.id} movie={movie}/>
+          })}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
--- src/Componets/Movie.js
+++ src/Componets/Movie.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import './Component.css';
+import { motion } from 'framer-motion';
 
 const Movie = ({movie}) => {
   return (
-    <div>
+    <motion.div 
+        animate={{ opacity: 1, scale: 1 }}
+        inherit={{ opacity: 0, scale: 0 }}
+        exit={{ opacity: 0, scale: 0 }}
+        layout
+    >
         <h2>{movie.title}</h2>
-        <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt="{movie.title}" />
-    </div>
+        <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt={movie.title} />
+    </motion.div>
   )
 }
 
